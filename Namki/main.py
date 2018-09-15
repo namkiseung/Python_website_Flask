ff81d6d8ef3e24dcb6f05020f03e1b98bcaca865
namki <rlzld100@gmail.com>
2018-09-15 00:53:05 -0700

    0915_16:53 fun(login,logout,register)

---
+++ Namki/main.py
@@ -0,0 +1,77 @@
+
+from flask import Flask, request, session, g, render_template
+import sqlite3, hashlib
+
+app = Flask(__name__)
+app.secret_key = 'a'
+DATABASE = './db/user.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        f = open('schema.sql', 'r')
+        db.execute(f.read())
+        db.commit()
+        db.close()
+
+def save_user(ar_id, ar_pw, ar_name, ar_email, ar_phone):
+    sql = 'INSERT INTO users (id, pw, name, email, phone) VALUES("{}", "{}", "{}", "{}", "{}")'.format(ar_id, ar_pw, ar_name, ar_email, ar_phone)
+    db = get_db()
+    db.execute(sql)
+    db.commit()
+    db.close()
+    return ''
+
+# def get_user(name, pw):
+#     sql = 'SELECT * FROM users where name="{}" and pw="{}"'.format(name, pw)
+#     db = get_db()
+#     rv = db.execute(sql)
+#     res = rv.fetchall()
+#     return res
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        logon_id = request.form.get('user_id','')
+        logon_pw = request.form.get('user_pw','')
+        print logon_id
+        print logon_pw        
+        session['logon'] = logon_id+logon_pw
+        print session['logon']
+        #session['user_email'] = ret[0][2]
+        if session['logon'] is not None:
+        #print '@@@@@@@@@@@'
+            return render_template('login.html', data=session['logon'], name=logon_id)
+    pass
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method=='GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        in_new_id = request.form.get('new_id', '')
+        tmp_in_new_pw = request.form.get('new_pw', '')
+        in_new_pw = hashlib.sha224(tmp_in_new_pw).hexdigest()
+        in_new_name = request.form.get('new_name', '')
+        in_new_email = request.form.get('new_email', '')
+        in_new_phone = request.form.get('new_phone', '')
+                
+        save_user(ar_id=in_new_id, ar_pw=in_new_pw, ar_name=in_new_name, ar_email=in_new_email, ar_phone=in_new_phone)
+        return '<a href="/">continue</a>'
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('logon')
+    return '<a href="/">continue</a>'
+
+if __name__ == '__main__':
+    #init_db()
+    app.run(debug=True, host='0.0.0.0', port=1111)
---
+++ Namki/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE users (id varchar(20), pw varchar(50), name varchar(20), email varchar(35), phone int);
