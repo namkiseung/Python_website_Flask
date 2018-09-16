f86e700d57babbfea661fd83995b07fb3867a28d
namkiseung <rlzld100@gmail.com>
2018-09-16 11:14:24 -0700

    null check and update & delete of users table

--- Namki/main.py
+++ Namki/main.py
@@ -1,10 +1,22 @@
-
 from flask import Flask, request, session, g, render_template
+from flask_basicauth import BasicAuth
 import sqlite3, hashlib
 
 app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = '0000'
+#'adc91e03060b42e7836bdfba7ce19b3bc1297d234fec44585472529d'
+basic_auth = BasicAuth(app)
+app.config['BASIC_AUTH_FORCE'] = False
 app.secret_key = 'a'
 DATABASE = './db/user.db'
+
+@app.route('/admin')
+@basic_auth.required
+def admin_access():
+    r = all_get_user()
+    return render_template('admin.html', user_info=r)
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -47,6 +59,28 @@ def hash_224(data):
     result = hashlib.sha224(data).hexdigest()
     return result
 
+def update_user(n_name=None, n_email=None, n_phone=None):
+    if n_name is not None and n_email is not None and n_phone is not None:
+       db = get_db()
+       #r = get_user()
+       print n_name
+       print n_email
+       print n_phone
+       sql = 'UPDATE users set name="{}", email="{}", phone="{}" where id="{}" '.format(n_name, n_email, n_phone, session['id']) 
+       rv = db.execute(sql)
+       db.commit()
+       db.close()
+    return ''
+
+def delete_user(bye_user):
+    db = get_db()
+    sql = 'DELETE FROM users where id="{}"'.format(bye_user)
+    logout()
+    rv = db.execute(sql)
+    db.commit()
+    db.close()
+    return ''
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
@@ -55,8 +89,11 @@ def index():
         logon_id = request.form.get('user_id','')
         logon_pw = request.form.get('user_pw','')
         session['id'] = logon_id
-        session['pw'] = logon_pw
-        #session['user_email'] = ret[0][2]
+        session['pw'] = logon_pw 
+        r=get_user(session['id'], session['pw'])
+        session['name'] = r[0][2]
+        session['email'] = r[0][3]
+        session['phone'] = r[0][4]
         if session['id'] is not None:
             return render_template('login.html', data=session['id'], name=logon_id)
     pass
@@ -77,16 +114,23 @@ def register():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    session.pop('id')
-    session.pop('pw')
+    if session['id'] is None:
+        return '<a href="/">continue</a>' 
+    for x in ['id', 'pw', 'name', 'email', 'phone']:
+        session.pop(x)
+        print x
     return '<a href="/">continue</a>'
+
+@app.route('/delete_user')
+def delete_user_action():
+    delete_user(bye_user=session['id'])
+    return '<h1>User Info Delete success!!</h1><br><a href="/">continue</a>'
 
 @app.route('/mypage', methods=['GET', 'POST'])
 def mypage():
-    a=all_get_user()
-    print a[0][0], a[0][1], a[0][2]
     if request.method == 'POST':
-        return request.form.get('name')+request.form.get('email')+ request.form.get('phone')
+        update_user(n_name=request.form.get('name'), n_email=request.form.get('email'), n_phone=str(request.form.get('phone')))
+        return '<h1>User Info Update success!!</h1><br><a href="/">continue</a>'
     if session['id'] is not None:
         r=get_user(session['id'], session['pw'])
         return render_template('mypage.html',update_id=r[0][0], update_pw=r[0][1], update_name=r[0][2], update_email=r[0][3], update_phone=r[0][4]) 
@@ -99,4 +143,5 @@ def main():
 
 if __name__ == '__main__':
     #init_db()
+    #init_auth()
     app.run(debug=True, host='0.0.0.0', port=1111)
---
+++ Namki/static/js/register_null.js
@@ -0,0 +1,11 @@
+function null_check(){
+	var str = document.register
+	if(str.new_id.value == '' || str.new_id.value == null){
+	alert('빈칸을 확인해주세요");
+	str.new_id.value.focus();
+	return;
+	}else{
+	}
+}
+window.alert('a');
+
